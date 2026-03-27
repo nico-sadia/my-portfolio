dd9079361e4aa53b01d796fc988791fd2c1e90f0
Nico Sadia <nicosadia17@gmail.com>
2026-03-27 13:51:50 +0000

    refactor: clean up way r2 assets are stored in constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,27 +1,47 @@
 // Assets
-
 export const R2_BASE_URL =
     "https://pub-280f09da048d460093e78e545200c0e6.r2.dev";
 
+const r2NicoAsset = (path: string) => `${R2_BASE_URL}/nico.dev/${path}`;
+
 export const ASSETS = {
     icons: {
-        nicoPfp: `${R2_BASE_URL}/nico.dev/icons/Nico-Sadia-PFP.jpg`,
-        spotifyPrimary: `${R2_BASE_URL}/nico.dev/icons/Spotify_Primary_Logo_Green.png`,
-        cronJob: `${R2_BASE_URL}/nico.dev/icons/cron-job-logo.jpg`,
+        nicoPfp: r2NicoAsset("icons/Nico-Sadia-PFP.jpg"),
+        spotifyPrimary: r2NicoAsset("icons/Spotify_Primary_Logo_Green.png"),
+        cronJob: r2NicoAsset("icons/cron-job-logo.jpg"),
         github: {
-            white: `${R2_BASE_URL}/nico.dev/icons/GitHub_Invertocat_White.svg`,
-            black: `${R2_BASE_URL}/nico.dev/icons/GitHub_Invertocat_Black.svg`,
+            white: r2NicoAsset("icons/GitHub_Invertocat_White.svg"),
+            black: r2NicoAsset("icons/GitHub_Invertocat_Black.svg"),
         },
     },
     diagrams: {
-        spotifySaverSequence: `${R2_BASE_URL}/nico.dev/diagrams/spotify-saver-sequence-diagram.svg`,
+        spotifySaverSequence: r2NicoAsset(
+            "diagrams/spotify-saver-sequence-diagram.svg",
+        ),
+    },
+    screenshots: {
+        cronJobCreationSettings: r2NicoAsset(
+            "screenshots/cron_job_creation_settings.png",
+        ),
+        spotifySaverDashboard: r2NicoAsset(
+            "screenshots/spotify-saver-dashboard.webp",
+        ),
+        spotifySaverHistory: r2NicoAsset(
+            "screenshots/spotify-saver-history.webp",
+        ),
+        spotifySaverLanding: r2NicoAsset(
+            "screenshots/spotify-saver-landing.webp",
+        ),
+        spotifySaverSettings: r2NicoAsset(
+            "screenshots/spotify-saver-settings.webp",
+        ),
     },
     pokemon: {
         pulls: {
             sv: {
                 mew: {
-                    venuPoli: `${R2_BASE_URL}/nico.dev/pokemon/IMG_1117.jpeg`,
-                    spread: `${R2_BASE_URL}/nico.dev/pokemon/IMG_1116.jpeg`,
+                    venuPoli: r2NicoAsset("pokemon/IMG_1117.jpeg"),
+                    spread: r2NicoAsset("pokemon/IMG_1116.jpeg"),
                 },
             },
         },
